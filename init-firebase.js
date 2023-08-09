fb10742d239dbb5c8a8284ac804e992b66859790
Zhejiej <139987987+Zhejiej@users.noreply.github.com>
2023-08-09 14:44:52 -0400

    Add files via upload

---
+++ init-firebase.js
@@ -0,0 +1,16 @@
+/**
+ * @TODO replace the firebaseConfig object below with the one 
+ * provided for you by firebase after creating your project!
+*/
+const firebaseConfig = {
+    apiKey: "AIzaSyAjlIdM9kEW0ZMS3sl7X7MRoylyYqQrzMU",
+    authDomain: "fire-message-d1fb5.firebaseapp.com",
+    projectId: "fire-message-d1fb5",
+    storageBucket: "fire-message-d1fb5.appspot.com",
+    messagingSenderId: "756853510801",
+    appId: "1:756853510801:web:da77cbd30529eea8f1e530",
+    measurementId: "G-7JLQ82L2WS"
+  };
+
+// initialize the firebase app with this built-in function
+firebase.initializeApp(firebaseConfig);
